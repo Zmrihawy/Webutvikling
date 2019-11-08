43ee8bdf6677938a0e7fa7d3ba8579f688a72a99
Rahimu Kasibante <rahimuk@stud.ntnu.no>
2019-11-09 00:31:42 +0100

    Issue #2: Added item lists frontend.

--- eCatalog/App.js
+++ eCatalog/App.js
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#D0D3D4',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
--- eCatalog/src/components/ListView.js
+++ eCatalog/src/components/ListView.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet  } from 'react-native';
 
-import { TextInput } from 'react-native-paper';
+import { Searchbar, List, Checkbox } from 'react-native-paper';
 export default class ListView extends React.Component {
     state = {
-        text: '',
+        firstQuery: '',
+        expanded: false,
     }
     
+    _handlePress = () =>
+        this.setState({
+            expanded: !this.state.expanded
+    });
+    
     render(){
+        const { firstQuery } = this.state;
         return (
             <View style={styles.margin}>
                 <View style={styles.center, styles.heading}>
@@ -15,10 +22,36 @@ export default class ListView extends React.Component {
                     <Text style={styles.small}>home for electronics</Text>
                 </View>
                 <View>
-                    <TextInput
-                        label='Search for a pc, mobile, TV ... '
-                        onChangeText={text => this.setState({ text })}
+                    <Searchbar
+                        placeholder='Search for a pc, mobile, TV ... '
+                        onChangeText={ text => this.setState({ firstQuery: query }) }
                     />
+                </View>
+                <View style={styles.body}>
+                    <List.Section>
+                
+                        <List.Accordion
+                               title="Item title"
+                               left={props => <List.Icon {...props} />}
+                               expanded={this.state.expanded}
+                               onPress={this._handlePress}
+                        >
+                               <List.Item title="Item title" />
+                               <List.Item title="Item Description" />
+                        </List.Accordion>
+                
+                        <List.Accordion
+                                title="Item title 2"
+                                left={props => <List.Icon {...props} />}
+                                expanded={this.state.expanded}
+                                onPress={this._handlePress}
+                        >
+                                <List.Item title="Item title 2" />
+                                <List.Item title="Item Description" />
+                        </List.Accordion>
+                
+                
+                    </List.Section>
                 </View>
             </View>
         );
@@ -27,8 +60,9 @@ export default class ListView extends React.Component {
 
 const styles = StyleSheet.create({
     margin: {margin: 35},
-    h1: {color: 'white', fontSize: 28},
+    h1: {fontSize: 28},
     center: {alignItems: 'center'},
-    small: {fontSize: 10, color: 'white'},
-    heading: {margin: 10}
+    small: {fontSize: 10},
+    heading: {margin: 10},
+    body:{color: 'white'},
 });
