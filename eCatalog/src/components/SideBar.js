9c68eb4cec721f5a1ce6c30743652abd8b5fcf7f
Rahimu Kasibante <rahimuk@stud.ntnu.no>
2019-11-15 15:21:14 +0100

    Issue #7: Updated Slide veiw.

--- eCatalog/src/components/SideBar.js
+++ eCatalog/src/components/SideBar.js
@@ -72,8 +72,12 @@ export default class SideBar extends React.Component {
         const { filter } = this.props;
         return (
             <Modal visible={filter === active} animationType={'slide'}>
-                <View style={{flex: 0.6,  alignItems: 'left', justifyContent: 'center', marginLeft: 40}}>
-                    <Text style={{fontSize: 25, marginBottom: 10}}> Filter by ... </Text>
+                <View style={{alignItems: 'center', marginTop: 50}}>
+                    <Text style={{fontSize: 28}}>eCatalog</Text>
+                    <Text style={{fontSize: 10}}>home for electronics</Text>
+                </View>
+                <View style={{grid: 0.8,  alignItems: 'left', justifyContent: 'center', marginTop: 10, marginLeft: 50, marginRight: 50, padding: 20, borderRadius: 8, backgroundColor: '#D0D3F4',}}>
+                    <Text style={{fontSize: 25, marginBottom: 10, marginTop: 2}}> Filter by ... </Text>
                     <View>
                         <View>
                             <DropDown name={'category'} data={category} onChange={() => { this.setState({ query: query + selectedItem });  }}/>
@@ -83,14 +87,14 @@ export default class SideBar extends React.Component {
                         <Text style={{fontSize: 25, marginBottom: 20, marginTop: 50}}> Sort by ... </Text>
                         <View style={{flexDirection: 'row'}}>
                             <View style={styles.checkbox} >
-                                <Checkbox status={ checked.checked1 ? 'checked' : 'unchecked'} onPress={() => { this.setState({ checked: {...checked, checked1: !checked.checked1 }}); }}/>
+                                <Checkbox color={'blue'} status={ checked.checked1 ? 'checked' : 'unchecked'} onPress={() => { this.setState({ checked: {...checked, checked1: !checked.checked1 }}); }}/>
                             </View>
                             <Text style={styles.chectext}>Price</Text>
                         </View>
                 
                         <View style={{flexDirection: 'row'}} >
                             <View style={styles.checkbox} >
-                                <Checkbox status={ checked.checked2 ? 'checked' : 'unchecked'} onPress={() => { this.setState({ checked: { ...checked, checked2: !checked.checked2 }}); }}/>
+                                <Checkbox color={'blue'} status={ checked.checked2 ? 'checked' : 'unchecked'} onPress={() => { this.setState({ checked: { ...checked, checked2: !checked.checked2 }}); }}/>
                             </View>
                             <Text style={styles.chectext}>Asc order </Text>
                         </View>
@@ -106,6 +110,6 @@ export default class SideBar extends React.Component {
 };
 
 const styles = StyleSheet.create({
-       checkbox: {height: 40, width: 40, borderWidth: 1.5, borderColor: 'grey', borderRadius: 3 , marginBottom: 6 },
+       checkbox: {height: 40, width: 40, borderWidth: 1.5, borderColor: 'grey', borderRadius: 3 , marginBottom: 12 },
        chectext: {margin: 8, fontSize: 18 }
 });
